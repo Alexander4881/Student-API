be1fae70e0e6f24f6ecf22b7d5a00c8afebbfe7e
goket1 <goket@protonmail.com>
2019-10-04 10:57:19 +0200

    Created LearningActivity and School classes

---
+++ Service-API/Data/Class/LearningActivity.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service_API.Data.Class
+{
+    class LearningActivity
+    {
+        uint userId;
+        string name;
+        uint schoolId;
+        DateTime startDate;
+        DateTime endDate;
+
+        public LearningActivity()
+        {
+
+        }
+
+        public LearningActivity(uint userId, uint schoolId)
+        {
+
+        }
+
+        public uint UserId
+        {
+            get
+            {
+                return userId;
+            }
+            private set
+            {
+                userId = value;
+            }
+        }
+
+        public uint SchoolId
+        {
+            get
+            {
+                return SchoolId;
+            }
+            private set
+            {
+                schoolId = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            private set
+            {
+                name = value;
+            }
+        }
+
+        public DateTime StartDate
+        {
+            get
+            {
+                return startDate;
+            }
+            set
+            {
+                startDate = value;
+            }
+        }
+
+        public DateTime EndDate { get => endDate; private set => endDate = value; }
+    }
+}
