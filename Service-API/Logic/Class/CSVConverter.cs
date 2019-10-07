5d54e714aa23823c6700bf3b151c4b078b6a43db
goket1 <goket@protonmail.com>
2019-10-07 09:23:47 +0200

    Begun working on CSV converter

---
+++ Service-API/Logic/Class/CSVConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Service_API.Logic
+{
+    class CSVConverter: IFullDataCopling
+    {
+        // attributes
+        private Thread CSVConverterThread;
+        private string path;
+
+        public CSVConverter(string path)
+        {
+            this.path = path;
+        }
+
+        public List<Object> Convert()
+        {
+            using (var reader = new StreamReader(@"C:\Users\user\Downloads\Praktikoversigt-120919-094327.csv"))
+            {
+                List<object> dataObjectsList = new List<object>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split('	');
+
+                    Console.WriteLine(values);
+                }
+            }
+        }
+    }
+}
